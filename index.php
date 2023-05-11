da72d84aec9953593f9e55bbdbee754743043845
Robin Devitt <iamrobindevitt@gmail.com>
2023-05-11 16:43:21 +0200

    setup index file with original docs

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Feel free to change any code.
+ *
+ * Please complete the following assessment.
+ *
+ * In the file below you will see an Object $data that has categories and products inside it.
+ *
+ * There are two functions we would like implemented:
+ * getProductsInCategory() - This function needs to return a product inside a category that we can specify.
+ * doesProductExistInCategory() - This function needs to let us know if a product exists in a category that we chose.
+ *
+ * Hints:
+ * Please make sure that the Object is extendable and other products and categories can be added and the functions will work.
+ * Big-O notation.
+ * Using DRY and SOLID principles.
+ * You can ask for help or more information at anytime.
+ * You can use any resources you like.
+ * The frontend is important.
+ *
+ * Bonus points:
+ * - PHP Unit Testing.
+ * - Frontend Display/Layout/Template to process data.
+ * - Git repository to track the code changes.
+ *
+ * You will be evaluated on the following:
+ * - Code structure, readability, understandably, maintainability and layout.
+ * - Code standards used.
+ * - Completion of the objective.
+ * - Performance of the completed structure.
+ * - Frontend Display/Layout/Template to process data using Javascript and CSS.
+ */
