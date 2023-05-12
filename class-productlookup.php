7e4b807e1cbd3b36819d12d0fb8aad63cbb72447
Robin Devitt <iamrobindevitt@gmail.com>
2023-05-12 11:43:04 +0200

    update rendering and ensure consistancy for a single product

--- class-productlookup.php
+++ class-productlookup.php
@@ -30,14 +30,21 @@ class ProductLookup {
 	 *
 	 * @param string $category_name Category name to look for.
 	 * @param array  $categories Catogries to check through.
+	 * @param string $product_name Product name to return.
 	 *
 	 * @return array
 	 */
-	public function get_products_in_category( string $category_name, array $categories ) {
-		$return_categories = array();
+	public function get_products_in_category( string $category_name, array $categories, string $product_name = '' ) {
 		foreach ( $categories as $category ) {
 			if ( $category->name === $category_name ) {
-				return array( $category );
+				if ( empty( $product_name ) ) {
+					return array( $category );
+				}
+				foreach ( $category->products as $product ) {
+					if ( $product->name === $product_name ) {
+						return $product;
+					}
+				}
 			}
 		}
 		return array();
@@ -49,14 +56,18 @@ class ProductLookup {
 	 * @param array  $categories Catogries to loop through.
 	 * @param string $category Category to render, leaving it blank shows all categories.
 	 */
-	public function render_products( array $categories, string $category = '' ) {
+	public function render_products( array $categories, string $category = '', string $product_name = '' ) {
 
 		if ( ! empty( $category ) ) {
-			$categories = $this->get_products_in_category( $category, $categories );
+			$categories = $this->get_products_in_category( $category, $categories, $product_name );
 		}
 
 		if ( empty( $categories ) ) {
-			return '<div id="render_wrapper" class="no_category">No category to show</div>';
+			return '<div id="render_wrapper" class="no_category">No category or products to show</div>';
+		}
+
+		if ( ! empty( $product_name ) && ! empty( $category ) ) {
+			return '<div id="render_wrapper" class="no_category">' . $this->product_render( (array) $categories ) . '</div>';
 		}
 
 		$html = '<div id="render_wrapper">';
@@ -72,9 +83,7 @@ class ProductLookup {
 			if ( isset( $category->products ) && ! empty( $category->products ) ) { // Check there are products before trying to render them.
 
 				foreach ( $category->products as $product ) { // Loop through each of the products.
-
-					$html .= $this->product_render( $product );
-
+					$html .= $this->product_render( (array) $product );
 				} // End of foreach loop.
 			} else { // Else show this message when there are no products.
 
@@ -94,20 +103,20 @@ class ProductLookup {
 	/**
 	 * Single Product render
 	 *
-	 * @param object $product Product Object.
+	 * @param array $product Product Array.
 	 */
-	public function product_render( object $product ) {
+	public function product_render( array $product ) {
 		$html = '<div class="product_wrapper">';
 			// Show the image if it's set.
-			$img   = ( isset( $product->image ) && ! empty( $product->image ) ? 'media/' . $product->image : 'assets/images/default.png' );
-			$html .= '<img width="240" height="240" alt="' . $product->name . '" src="' . $img . '" />';
+			$img   = ( isset( $product['image'] ) && ! empty( $product['image'] ) ? 'media/' . $product['image'] : 'assets/images/default.png' );
+			$html .= '<img width="240" height="240" alt="' . $product['name'] . '" src="' . $img . '" />';
 
 			// Product name.
-			$html .= '<h3 class="product_title">' . $product->name . '</h3>';
+			$html .= '<h3 class="product_title">' . $product['name'] . '</h3>';
 
 			// Show Pricing if it's set.
-		if ( isset( $product->price ) ) {
-			$html .= '<div class="product_price">' . $product->price . '</div>';
+		if ( isset( $product['price'] ) ) {
+			$html .= '<div class="product_price">' . $product['price'] . '</div>';
 		}
 
 		$html .= '</div>';
